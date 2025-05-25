e855024083252cec4fb8077b971d1a5901b9dfe6
Bharath <bharath333e@gmail.com>
2025-05-26 04:16:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -63,7 +63,7 @@ AUTO_DEL_SUCCESS_MSG = os.environ.get("AUTO_DEL_SUCCESS_MSG", "Your file has bee
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "If you have any problems or need anything kindly report it in our group @BorutoTamil"
+USER_REPLY_TEXT = "If you have any problems in the episodes or need any other animes inform it in our group @BorutoTamil"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
